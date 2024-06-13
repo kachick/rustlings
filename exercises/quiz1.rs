5d5b030cf08bcfa42e1a7f30cc3e5a1a1e6c4d59
Kenichi Kamiya <kachick1@gmail.com>
2024-06-13 16:08:37 +0900

    Complete quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(volumes: i32) -> i32 {
+    if volumes > 40 {
+        return volumes
+    } else {
+        return volumes * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
